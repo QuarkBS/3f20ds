92f53c289943deaf422a787fba8a36555a4f4bbf
QuarkBS <72528875+QuarkBS@users.noreply.github.com>
2020-10-26 20:14:40 -0600

    Update taylor.js

--- 1p/ej/series/taylor.js
+++ 1p/ej/series/taylor.js
@@ -3,17 +3,18 @@ let x = Math.PI/2, seno = 0
 //let a = Math.sin(Math.PI/2)
 //console.log('El valor real es: ',a)
 
-for(k = 0; k <= 10; k++){
+for(let k = 0; k <= 10; k++){
     calcularfactorial()
-    semi = Math.pow(-1,k)*(Math.pow(x,2*k+1)/calcularfactorial(2*k+1))
+    let semi = Math.pow(-1,k)*(Math.pow(x,2*k+1)/calcularfactorial(2*k+1))
     seno += semi
-    console.log('El valor de la iteración es: ',seno)
+    console.log(semi)
+    //console.log('El valor de la suma en la iteración es: ',seno)
 }
 //console.log('El valor aproximado final es: ',seno)
 
 function calcularfactorial(n){
     let factorial = 1
-    for(i = 1; i <= n; i++){
+    for(let i = 1; i <= n; i++){
           factorial = i * factorial
     }
     return factorial
