fcbf10713bba0beb64e3be82e22c056a03cf6b82
QuarkBS <72528875+QuarkBS@users.noreply.github.com>
2020-11-06 23:47:24 -0600

    Add files via upload

---
+++ 1p/ej/iterativo/10e5.js
@@ -0,0 +1,43 @@
+let x = [], min = 0, max = 99999, cantidad = 10, z = 0, p = 6, inversiones, totalinversiones = 0;
+
+//Use 6 veces la función para calcular los promedios
+
+for(let k = 0; k < p; k++){
+    random();
+    let v = new Date().getTime();
+    console.log(burbuja());
+    let y = new Date().getTime();
+    z += y-v;
+    totalinversiones += inversiones;    
+}
+
+console.log('La cantidad de inversiones promedio son: ',totalinversiones/p);
+console.log('El promedio de tiempo de ejecución es de: ',z/p,'milisegundos');
+console.log('¿Se puede mejorar el tiempo de ejecución? Sí, usando otro método de ordenamiento xD');
+
+function cambiar(n, m){
+    let temp = x[n];
+    x[n] = x[m];
+    x[m] = temp;
+}
+
+function burbuja(){
+    inversiones = 0;
+    for(let i = 0; i < cantidad; i++){
+        for(let j = 0; j < cantidad-1; j++){
+            if(x[j] > x[j+1]){
+                cambiar(j, j+1);
+                inversiones++
+            }
+            else continue;
+        }
+    }
+    return x;
+}
+
+function random(){
+    for (k = 0; k < cantidad; k++){
+        x[k] = Math.floor(Math.random() * max + min);
+    }
+    return x;
+}
